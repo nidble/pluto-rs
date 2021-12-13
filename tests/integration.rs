d3443ef7948a7148f0cd7b2ea759eda667170b57
Antonino Bertulla <abertulla@gmail.com>
2021-12-13 23:31:53 +0100

    Add Healthcheck test and refactoring

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,36 +1,38 @@
-use pluto_rs::actions;
-use pluto_rs::model;
-
 #[cfg(test)]
 mod tests {
     use dotenv::dotenv;
-    use pluto_rs::api::Currency;
+    use pluto_rs::init_routes;
     use rweb::test::request;
-    use sqlx::postgres::PgPoolOptions;
-
-    use super::actions::new_exchange;
-    use super::model::ExchangeRepository;
 
     #[tokio::test]
-    async fn should_return_200() {
+    async fn new_exchanges_should_return_201() {
         dotenv().ok();
-        let database_url = std::env::var("DATABASE_URL").unwrap();
 
-        let pool = PgPoolOptions::new()
-            .max_connections(1)
-            .connect(&database_url)
-            .await
-            .unwrap();
+    let routes = init_routes(1).await.unwrap();
 
-        let api = new_exchange(ExchangeRepository::new(pool).clone(), Currency::new());
         let body = r#"{"currencyFrom": "EUR", "currencyTo": "USD", "amountFrom": 123, "createdAt": "2012-04-23T18:25:43.511Z"}"#;
         let res = request()
             .method("POST")
             .body(body)
             .path("/exchanges")
-            .reply(&api)
+            .reply(&routes)
             .await;
 
         assert_eq!(res.status(), 201, "POST works");
+    }
+
+    #[tokio::test]
+    async fn healthcheck_should_return_200() {
+        dotenv().ok();
+
+        let routes = init_routes(1).await.unwrap();
+
+        let res = request()
+            .method("GET")
+            .path("/healthz")
+            .reply(&routes)
+            .await;
+
+        assert_eq!(res.status(), 200, "POST works");
     }
 }
